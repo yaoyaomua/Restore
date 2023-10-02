f381d780e62fb208421317e529f87d4a4dc88bc1
username <48670274+yaoyaomua@users.noreply.github.com>
2023-10-02 22:29:23 +0200

    End of section3

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddDbContext<StoreContext>(opt =>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,6 +23,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(opt => {
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+});
 
 app.UseAuthorization();
 
